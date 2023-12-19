349b0e2c62aa9766dc8b8a0fcbaab8faeb6634be
tiago <tiagopinzgraffunder@gmail.com>
2023-12-19 01:52:27 -0300

    adcionando landing page

---
+++ js/dist/stars.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var numberSpans = 60; // local onde o span será gerado
+
+var container = document.getElementsByClassName("containerIndex-stars")[0]; // loop parar gerar a quantidade de spans
+
+for (var i = 0; i < numberSpans; i++) {
+  // cria o span
+  var span = document.createElement("span"); // adiciona a classe ao span
+
+  span.className = "containerIndex-stars-star"; // Adiciona algum conteúdo ao span (opcional)
+
+  span.style.setProperty("--i", Math.random() * 15); // add o snap ao container
+
+  container.appendChild(span);
+}
