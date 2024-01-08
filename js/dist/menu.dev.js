78371dcd0f775696205fa91bef62aff0b607a864
tiago <tiagopinzgraffunder@gmail.com>
2024-01-08 11:28:02 -0300

    estilizando o menu nav-bar

--- js/dist/menu.dev.js
+++ js/dist/menu.dev.js
@@ -3,7 +3,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   var menuIcon = document.getElementById("menuHamburguer");
   var menuList = document.getElementById("menuList");
+  var menuImg = document.getElementById("menuImg");
+  var isXImage = false;
   menuIcon.addEventListener("click", function () {
     menuList.classList.toggle("show");
+    menuImg.style.margin = "20px 0";
+  });
+  menuImg.addEventListener("click", function () {
+    if (!isXImage) {
+      menuImg.src = "../img/x.png";
+    } else {
+      menuImg.src = "../img/menu-bar.jpg";
+    }
+
+    isXImage = !isXImage;
   });
 });
--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.getElementById("menuHamburguer");
   const menuList = document.getElementById("menuList");
+  const menuImg = document.getElementById("menuImg");
+  let isXImage = false;
 
   menuIcon.addEventListener("click", function () {
     menuList.classList.toggle("show");
+    menuImg.style.margin = "20px 0";
+  });
+
+  menuImg.addEventListener("click", function () {
+    if (!isXImage) {
+      menuImg.src = "../img/x.png";
+    } else {
+      menuImg.src = "../img/menu-bar.jpg";
+    }
+
+    isXImage = !isXImage;
   });
 });
