6b18d7d9b25822abdeb7912f74d5571440a40d7c
tiago <tiagopinzgraffunder@gmail.com>
2024-01-19 11:08:14 -0300

    adcionando estilo ao conclusão do curso e projetos

---
+++ js/conclusion.js
@@ -0,0 +1,4 @@
+const percentage = 58.18;
+const process = document.querySelector(".main-graduation-list-item");
+process.setAttribute("style", "width: " + percentage + "%");
+process.setAttribute("title", percentage + "%");
---
+++ js/dist/conclusion.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var percentage = 58.18;
+var process = document.querySelector(".main-graduation-list-item");
+process.setAttribute("style", "width: " + percentage + "%");
+process.setAttribute("title", percentage + "%");
